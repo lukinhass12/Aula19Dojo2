231c147a522553f28880a21c8d9a5bba916691cf
Your Name <you@example.com>
2020-06-17 15:33:07 -0300

    Aula19Dojo2

---
+++ Ingresso.cs
@@ -0,0 +1,11 @@
+namespace Aula19Dojo2
+{
+    public class Ingresso
+    {
+        public float IngressoPreco {get; set;}
+
+        public void ImprimirValor(){
+            System.Console.WriteLine("O valor do ingresso é:"+ IngressoPreco);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿  
+using System;
+
+namespace Aula19Dojo2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            IngressoVip passagem = new IngressoVip();
+            passagem.IngressoPreco = 10.00f;
+            passagem.ImprimirValor();
+            passagem.AdicionarValor();
+
+            System.Console.WriteLine("A diferença de valores é: " + passagem.valorAdicional + "reais.");
+            
+        }
+    }
+}
